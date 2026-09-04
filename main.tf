36829e67a4da0c3a7d647123f98a173afa13c708
bala <balanandan403@gmail.com>
2026-09-04 18:22:17 +0530

    commit

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ output "ip" {
 }
 
 data "azurerm_subnet" "default" {
-  name                 = "vnet-denmarkeast-2"
+  name                 = "vnet-denmarkeast-1"
   virtual_network_name = "workstation-vnet"
   resource_group_name  = "Nothing"
 }
